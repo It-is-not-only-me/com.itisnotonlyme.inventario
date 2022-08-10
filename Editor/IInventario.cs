6494e52f9b1fc764a2a7c20aff42518c2720a305
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-08-10 15:54:35 -0300

    Avanzando en la idea del inventario

--- Editor/IInventario.cs
+++ Editor/IInventario.cs
@@ -6,6 +6,8 @@ namespace ItIsNotOnlyMe.Inventario
 {
     public interface IInventario
     {
+        public void AgregarEspacio(IEspacio espacio);
+
         public bool Agregar(IElemento elemento);
 
         public bool TieneEspacio();
@@ -13,29 +15,56 @@ namespace ItIsNotOnlyMe.Inventario
 
     public class Inventario : IInventario
     {
+        List<IEspacio> _espacios;
+
         public Inventario()
         {
+            _espacios = new List<IEspacio>();
+        }
 
+        public void AgregarEspacio(IEspacio espacio)
+        {
+            _espacios.Add(espacio);
         }
 
         public bool Agregar(IElemento elemento)
         {
-            return false;
+            bool agregado = false;
+
+            for (int i = 0; i < _espacios.Count && !agregado; i++)
+            {
+                IEspacio espacio = _espacios[i];
+                if (!espacio.TieneEspacio(elemento))
+                    continue;
+
+                agregado = espacio.Agregar(elemento);                                    
+            }
+
+            return agregado;
         }
 
         public bool TieneEspacio()
         {
-            return false;
+            bool tieneEspacio = false;
+
+            foreach (IEspacio espacio in _espacios)
+                tieneEspacio |= espacio.TieneEspacio();
+
+            return tieneEspacio;
         }
     }
 
-    public interface ISlot
+    public interface IEspacio
     {
+        public bool Agregar(IElemento elemento);
 
+        public bool TieneEspacio();
+
+        public bool TieneEspacio(IElemento elemento);
     }
 
     public interface IElemento
     {
-
+        public bool EsIgual(IElemento elemento);
     }
 }
--- Tests/Editor/InventarioTest.cs
+++ Tests/Editor/InventarioTest.cs
@@ -7,11 +7,86 @@ using ItIsNotOnlyMe.Inventario;
 
 public class ElementoPrueba : IElemento
 {
+    private int _id;
 
+    public ElementoPrueba(int id)
+    {
+        _id = id;
+    }
+
+    public bool EsIgual(IElemento elemento)
+    {
+        ElementoPrueba elementoPrueba = elemento as ElementoPrueba;
+        return elementoPrueba._id == _id;
+    }
+}
+
+public class SlotInfinitoPrueba : IEspacio
+{
+    public List<IElemento> _elementos;
+
+    public SlotInfinitoPrueba()
+    {
+        _elementos = new List<IElemento>();
+    }
+
+    public bool Agregar(IElemento elemento)
+    {
+        _elementos.Add(elemento);
+        return true;
+    }
+
+    public bool TieneEspacio()
+    {
+        return true;
+    }
+
+    public bool TieneEspacio(IElemento elemento)
+    {
+        return true;
+    }
+}
+
+public class SlotEspecificoPrueba : IEspacio
+{
+    public IElemento _elementoEspecifico;
+    public List<IElemento> _elementos;
+
+    public SlotEspecificoPrueba(IElemento elementoEspecifico)
+    {
+        _elementoEspecifico = elementoEspecifico;
+        _elementos = new List<IElemento>();
+    }
+
+    public bool Agregar(IElemento elemento)
+    {
+        if (!TieneEspacio(elemento))
+            return false;
+        _elementos.Add(elemento);
+        return true;
+    }
+
+    public bool TieneEspacio()
+    {
+        return true;
+    }
+
+    public bool TieneEspacio(IElemento elemento)
+    {
+        return _elementoEspecifico.EsIgual(elemento);
+    }
 }
 
 public class InventarioTest
 {
+    private IElemento _elementoPrincipal, _elementoDiferente;
+
+    public InventarioTest()
+    {
+        _elementoPrincipal = new ElementoPrueba(1);
+        _elementoDiferente = new ElementoPrueba(2);
+    }
+
     [Test]
     public void Test01InventarioEmpiezaSinEspacio()
     {
@@ -24,14 +99,29 @@ public class InventarioTest
     public void Test02InventarioSinEspacioNoPuedeAgregarNada()
     {
         IInventario inventario = new Inventario();
-        IElemento elemento = new ElementoPrueba();
 
-        Assert.IsFalse(inventario.Agregar(elemento));
+        Assert.IsFalse(inventario.Agregar(_elementoPrincipal));
     }
 
     [Test]
-    public void Test03()
+    public void Test03InventarioAgregandoleUnEspacioTieneEspacio()
     {
+        IInventario inventario = new Inventario();
+        IEspacio slotInfinito = new SlotInfinitoPrueba();
 
+        inventario.AgregarEspacio(slotInfinito);
+
+        Assert.IsTrue(inventario.TieneEspacio());
+    }
+
+    [Test]
+    public void Test04InventarioConEspacioSeAgregaSePuedeAgregarUnElemento()
+    {
+        IInventario inventario = new Inventario();
+        IEspacio slotInfinito = new SlotInfinitoPrueba();
+
+        inventario.AgregarEspacio(slotInfinito);
+
+        Assert.IsTrue(inventario.Agregar(_elementoPrincipal));
     }
 }
